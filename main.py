16fe462bdfbc6ab1d9bd0dbc8216138637e4fefe
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-07-23 17:51:44 +0530

    feat : update metadata on repos

--- main.py
+++ main.py
@@ -22,20 +22,14 @@ def get_commits(username: str):
     repos = sorted(user.get_repos(), key=lambda r: r.updated_at, reverse=True)
     for repo in repos:
         try:
-            # commits = repo.get_commits()
-            # for commit in commits[:5]:
-            #     commits_summary.append({
-            #         "repo": repo.name,
-            #         "message": commit.commit.message,
-            #         "date": commit.commit.author.date.isoformat()
-            #     })
-            
             local_created = repo.created_at.astimezone(ZoneInfo(time_zone))
             local_updated = repo.updated_at.astimezone(ZoneInfo(time_zone))
+            commit_count = repo.get_commits(sha="main").totalCount
             commits_summary.append({
                 "repo": repo.name,
                 "created_at": local_created.isoformat(),
-                "update_at": local_updated.isoformat()
+                "update_at": local_updated.isoformat(),
+                "commit_count_on_main": commit_count
             })
         except:
             continue
