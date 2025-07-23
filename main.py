1ae9e4250eabf5a1cbcb917cddb83e244aeba0a3
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2025-07-23 15:46:21 +0530

    feat : accessing repo metadata

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ def get_commits(username: str):
     commits_summary = []
     for repo in user.get_repos():
         try:
-            commits = repo.get_commits()
-            for commit in commits[:5]:
-                commits_summary.append({
-                    "repo": repo.name,
-                    "message": commit.commit.message,
-                    "date": commit.commit.author.date.isoformat()
-                })
+            # commits = repo.get_commits()
+            # for commit in commits[:5]:
+            #     commits_summary.append({
+            #         "repo": repo.name,
+            #         "message": commit.commit.message,
+            #         "date": commit.commit.author.date.isoformat()
+            #     })
+            commits_summary.append({
+                "repo": repo.name,
+                 "created_at": repo.created_at.isoformat()
+            })
         except:
             continue
     return commits_summary
