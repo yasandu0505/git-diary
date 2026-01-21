b5dec4cce09d06c7e7060068c7fbc538f37098f7
Yasandu Imanjith <89386702+yasandu0505@users.noreply.github.com>
2026-01-21 20:26:46 +0530

    initial commit

---
+++ web-app/proxy.ts
@@ -0,0 +1,10 @@
+import {asgardeoMiddleware} from '@asgardeo/nextjs/server';
+
+export const proxy = asgardeoMiddleware();
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
